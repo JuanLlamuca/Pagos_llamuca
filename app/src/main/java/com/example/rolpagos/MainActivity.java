fdd3f7ba3f4cd050ec0a8be0930f8a71fc78eb1e
Juan Llamuca <120286570+JuanLlamuca@users.noreply.github.com>
2023-06-08 16:29:37 -0500

    funciones de MainActivity

--- app/src/main/java/com/example/rolpagos/MainActivity.java
+++ app/src/main/java/com/example/rolpagos/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.rolpagos;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText fo_funcionario, fo_cargo, fo_area, fo_hijo,fo_estado;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //UNIR ELEMENT0S DE BACK CON FRONT
+        fo_funcionario=findViewById(R.id.txt_formulario);
+        fo_cargo=findViewById(R.id.txt_cargo);
+        fo_area=findViewById(R.id.txt_area);
+        fo_hijo=findViewById(R.id.txt_nroHijos);
+        fo_estado=findViewById(R.id.txt_estadoCivil);
     }
+
 }
