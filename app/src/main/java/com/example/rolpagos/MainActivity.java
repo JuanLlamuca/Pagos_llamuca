2648aa5579ad526d10317db2b2913ec6e9b68d0c
Juan Llamuca <120286570+JuanLlamuca@users.noreply.github.com>
2023-06-08 16:55:00 -0500

    funcion de insercion

--- app/src/main/java/com/example/rolpagos/MainActivity.java
+++ app/src/main/java/com/example/rolpagos/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.rolpagos;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.ContentValues;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.example.rolpagos.bdd.BDHelper;
 
 public class MainActivity extends AppCompatActivity {
 
-    EditText fo_funcionario, fo_cargo, fo_area, fo_hijo,fo_estado;
+    EditText fo_funcionario, fo_cargo, fo_area, fo_hijo,fo_estado, fo_extras,fo_retraso,fo_total;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -16,8 +22,38 @@ public class MainActivity extends AppCompatActivity {
         fo_funcionario=findViewById(R.id.txt_formulario);
         fo_cargo=findViewById(R.id.txt_cargo);
         fo_area=findViewById(R.id.txt_area);
-        fo_hijo=findViewById(R.id.txt_nroHijos);
-        fo_estado=findViewById(R.id.txt_estadoCivil);
+        fo_hijo=findViewById(R.id.txt_hijos);
+        fo_estado=findViewById(R.id.txt_estado);
+    }
+
+    public void registrar(View view){
+        BDHelper admin=new BDHelper(this,"registro.db",null,1);
+        SQLiteDatabase bd=admin.getWritableDatabase();
+        String funcionario=fo_funcionario.getText().toString();
+        String cargo=fo_cargo.getText().toString();
+        String area=fo_area.getText().toString();
+        String hijo=fo_hijo.getText().toString();
+        String estado=fo_estado.getText().toString();
+
+        if(!funcionario.isEmpty() && !cargo.isEmpty() && !area.isEmpty() && !hijo.isEmpty() && !estado.isEmpty()){
+            ContentValues registro=new ContentValues();
+            registro.put("for_funcionario",funcionario);
+            registro.put("for_cargo",cargo);
+            registro.put("for_area",area);
+            registro.put("for_hijo",hijo);
+            registro.put("for_estado",estado);
+            bd.insert("tblFormulario",null,registro);
+            Toast.makeText(this, "REGISTRO EXITOSO", Toast.LENGTH_SHORT).show();
+            fo_funcionario.setText("");
+            fo_area.setText("");
+            fo_cargo.setText("");
+            fo_hijo.setText("");
+            fo_estado.setText("");
+            bd.close();
+        }else{
+            Toast.makeText(this,"FAVOR INGRESAR TODOS LOS CAMPOS",Toast.LENGTH_SHORT).show();
+        }
+
     }
 
 }
--- app/src/main/java/com/example/rolpagos/bdd/BDHelper.java
+++ app/src/main/java/com/example/rolpagos/bdd/BDHelper.java
@@ -1,27 +1,23 @@
 package com.example.rolpagos.bdd;
-
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
-import android.os.Bundle;
-
-import com.example.rolpagos.R;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
 
 public class BDHelper extends AppCompatActivity {
 
     public BDHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
-        super(funcionario, cargo, area, hijos, estado_civil);
+        super(context, name, factory, version);
     }
+
 
     public void onCreate(SQLiteDatabase db) {
         //CREACIÓN DE LAS TABLAS
-        db.execSQL("CREATE TABLE tblFormulario"+"(" +
-                "for_id INTEGER PRIMARY KEY AUTOINCREMENT,"+
-                "for_funcionario text NOT NULL,"+
+        db.execSQL("CREATE TABLE tblFormulario" + "(" +
+                "for_id INTEGER PRIMARY KEY AUTOINCREMENT," +
+                "for_funcionario text NOT NULL," +
                 "for_cargo text NOT NULL," +
-                "for_area integer NOT NULL,"+
+                "for_area integer NOT NULL," +
                 "for_hijos text NOT NULL," +
                 "for_civil text NOT NULL)");
 
@@ -32,5 +28,6 @@ public class BDHelper extends AppCompatActivity {
         db.execSQL("DROP TABLE tblFormulario");
         onCreate(db);
     }
+}
 
 }
